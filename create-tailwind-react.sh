26c2f2d099941ec4a9b9ca65201c9945e8a12c12
Ulric Todman <rick.sinclair.nyc@gmail.com>
2023-03-30 21:44:52 -0400

    Update create-tailwind-react.sh

--- create-tailwind-react.sh
+++ create-tailwind-react.sh
@@ -1,48 +1,40 @@
 #!/bin/bash
 
-MAX_PROJECT_NAME_LENGTH=256
-
-if [ "$#" -lt 1 ]; then
+if [ -z "$1" ]; then
     echo "No arguments supplied"
     exit 1
 fi
 
-if [ "${#1}" -gt "$MAX_PROJECT_NAME_LENGTH" ]; then
-    echo "Project name is too long. Maximum length is $MAX_PROJECT_NAME_LENGTH characters."
-    exit 1
-fi
-
 npm create vite@latest "$1" -- --template react-ts
-cd "$1" && npm install -D tailwindcss postcss autoprefixer && npx tailwindcss init -p
+cd "$1" || exit
 
-# Remove the existing src/index.css file
-rm src/index.css
+npm install -D tailwindcss postcss autoprefixer sass
+npx tailwindcss init -p
 
-# Create a new src/index.css file with the specified contents
-printf '@tailwind base;\n@tailwind components;\n@tailwind utilities;' > src/index.css
-
-# Remove the existing tailwind.config.cjs file
-rm tailwind.config.cjs
-
-# Recreate the tailwind.config.cjs file with the provided content
-cat > tailwind.config.cjs << EOL
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+echo 'module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx,scss,css}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-EOL
+};' > tailwind.config.js
 
-echo "The new tailwind.config.cjs file has been created with the specified contents."
-echo "The new ./src/index.css file has been created with the specified contents."
+echo '@import "tailwindcss/base";' > ./src/index.scss
+echo '@import "tailwindcss/components";' >> ./src/index.scss
+echo '@import "tailwindcss/utilities";' >> ./src/index.scss
+
+echo 'export default defineConfig({
+  plugins: [
+    react(),
+    scss({
+      sass: require("sass")
+    }),
+  ],
+});' > vite.config.js
+
+echo "The new tailwind.config.js file has been created with the specified contents."
+echo "The new ./src/index.scss file has been created with the specified contents."
+echo "The new vite.config.js file has been created with the specified contents."
 echo "Start using Tailwind's utility classes to style your content."
-
-# Invoke the WebStorm command
-webstorm "$1"
-
