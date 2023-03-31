501981f6850224648a10598035e5463a9e6b8e29
Ulric Todman <rick.sinclair.nyc@gmail.com>
2023-03-30 21:44:29 -0400

    Update create-tailwind-react.c

--- create-tailwind-react.c
+++ create-tailwind-react.c
@@ -2,16 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_PROJECT_NAME_LENGTH 256
-
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         printf("No arguments supplied\n");
-        return 1;
-    }
-
-    if (strlen(argv[1]) > MAX_PROJECT_NAME_LENGTH) {
-        printf("Project name is too long. Maximum length is %d characters.\n", MAX_PROJECT_NAME_LENGTH);
         return 1;
     }
 
@@ -20,49 +13,25 @@ int main(int argc, char *argv[]) {
     snprintf(command, sizeof(command), "npm create vite@latest %s -- --template react-ts", argv[1]);
     system(command);
 
-    snprintf(command, sizeof(command), "cd %s && npm install -D tailwindcss postcss autoprefixer && npx tailwindcss init -p", argv[1]);
+    snprintf(command, sizeof(command), "cd %s && npm install -D tailwindcss postcss autoprefixer sass", argv[1]);
     system(command);
 
-    // Remove the existing src/index.css file
-    snprintf(command, sizeof(command), "rm %s/src/index.css", argv[1]);
+    snprintf(command, sizeof(command), "cd %s && npx tailwindcss init -p", argv[1]);
     system(command);
 
-    // Create a new src/index.css file with the specified contents
-    snprintf(command, sizeof(command), "printf '@tailwind base;\\n@tailwind components;\\n@tailwind utilities;' > %s/src/index.css", argv[1]);
+    snprintf(command, sizeof(command), "cd %s && echo 'module.exports = {\\n  content: [\\n    \"./src/**/*.{js,jsx,ts,tsx,scss,css}\"\\n  ],\\n  theme: {\\n    extend: {},\\n  },\\n  plugins: [],\\n};' > tailwind.config.js", argv[1]);
     system(command);
 
-    // Replace the content of tailwind.config.cjs with the provided content
-    char tailwind_config_file_path[1024];
-    snprintf(tailwind_config_file_path, sizeof(tailwind_config_file_path), "%s/tailwind.config.js", argv[1]);
+    snprintf(command, sizeof(command), "cd %s && echo '@import \"tailwindcss/base\";\\n@import \"tailwindcss/components\";\\n@import \"tailwindcss/utilities\";' > ./src/index.scss", argv[1]);
+    system(command);
 
-    FILE *file = fopen(tailwind_config_file_path, "w");
+    snprintf(command, sizeof(command), "cd %s && echo 'export default defineConfig({\\n  plugins: [\\n    react(),\\n    scss({\\n      sass: require(\'sass\')\\n    }),\\n  ],\\n});' > vite.config.js", argv[1]);
+    system(command);
 
-    if (file) {
-        fputs("/** @type {import('tailwindcss').Config} */\n", file);
-        fputs("module.exports = {\n", file);
-        fputs("  content: [\n", file);
-        fputs("    \"./index.html\",\n", file);
-        fputs("    \"./src/**/*.{js,ts,jsx,tsx}\",\n", file);
-        fputs("  ],\n", file);
-        fputs("  theme: {\n", file);
-        fputs("    extend: {},\n", file);
-        fputs("  },\n", file);
-        fputs("  plugins: [],\n", file);
-        fputs("}\n", file);
-
-        fclose(file);
-    } else {
-        printf("Failed to open the tailwind.config.cjs file for writing.\n");
-        return 1;
-    }
-
-    printf("The new tailwind.config.cjs file has been created with the specified contents.\n");
-    printf("The new ./src/index.css file has been created with the specified contents.\n");
+    printf("The new tailwind.config.js file has been created with the specified contents.\n");
+    printf("The new ./src/index.scss file has been created with the specified contents.\n");
+    printf("The new vite.config.js file has been created with the specified contents.\n");
     printf("Start using Tailwind's utility classes to style your content.\n");
-
-    // Invoke the WebStorm command
-    snprintf(command, sizeof(command), "webstorm %s", argv[1]);
-    system(command);
 
     return 0;
 }
