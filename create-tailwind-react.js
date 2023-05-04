922ceedc5d6f624612f1d2b2cd5ed44ad3cb2d6c
Ulric Todman <rick.sinclair.nyc@gmail.com>
2023-05-04 12:48:54 -0400

    changed button styling for cleaner look

--- create-tailwind-react.js
+++ create-tailwind-react.js
@@ -108,7 +108,7 @@ function App() {
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <button
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="rounded-md bg-cyan-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600"
                 data-about="This is a great book on how Tailwind is a design system. Highly recommended."
                 onClick={() =>
                   window.open('https://RefactoringUI.com', '_blank')
